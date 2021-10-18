53b04d7942b43f8116f511d778e076e52084ef01
Harry Baldwin <harrythree@gmail.com>
2021-10-17 23:16:57 -0400

    adding game moves and winning logic

--- src/rooms/TicTacToe.ts
+++ src/rooms/TicTacToe.ts
@@ -3,6 +3,17 @@ import { State, Player } from './schema/TicTacToeState';
 
 // Game logic taken from: https://github.com/BornaSepic/Tic-Tac-Toe/blob/master/script.js
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 export class TicTacToe extends Room<State> {
   maxClients = 2;
 
